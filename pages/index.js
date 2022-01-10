1aedffc9789f2ed43086d844e7b290128ea60a18
AnujCtrl <anujpatil200@gmail.com>
2022-01-10 07:20:28 +0530

    added todos modal

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
-import { Box } from "@chakra-ui/react";
+import { useDisclosure } from "@chakra-ui/hooks";
 import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
+import ManageTodo from "../components/ManageTodo";
+import Navbar from "../components/Navbar";
+import { supabaseClient } from "../lib/client";
 
 const Home = () => {
+  const initialRef = useRef();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const router = useRouter();
+  const user = supabaseClient.auth.user();
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/signin");
+    }
+  }, [user, router]);
+
   return (
     <div>
       <Head>
@@ -13,7 +30,8 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Box>Hello world</Box>
+        <Navbar onOpen={onOpen} />
+        <ManageTodo isOpen={isOpen} onClose={onClose} initialRef={initialRef} />
       </main>
     </div>
   );
